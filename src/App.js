99206a396231fc069dd444e3ccd0d0bb1e0fc5b5
mizaelgoonfs <miza14pg@hotmail.com>
2021-06-07 13:12:54 -0300

    change paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Exemplo de CI e CD com GitHub Actions
         </p>
         <a
           className="App-link"
